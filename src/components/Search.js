bd637ff24a6dccfdac13f34f646d1ae5cd529365
Max Miller <maxmiller413@gmail.com>
2021-05-01 17:22:09 -0500

    before api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Search(){
+
+    const [restaurant, setRestaurant] = useState([])
+    const [location, setLocation] = useState([])
+
+    function onhandleSubmit(e){
+        e.preventDefault()
+        const restaurantSearch = {
+            restaurant,
+            location
+        }
+        console.log(restaurantSearch)
+    }
+
     return(
-        <h1> Search </h1>
+        <div>
+            <h1> Search </h1>
+            <form onSubmit={onhandleSubmit}>
+                <label> Restaurant </label>
+                <input 
+                type="text"
+                id="restaurant"
+                name="restaurant"
+                placeholder="Search by cuisine!"
+                value={restaurant}
+                onChange={e => setRestaurant(e.target.value)}
+                />
+
+            <label> Location </label>
+                <input 
+                type="text"
+                id="location"
+                name="location"
+                placeholder="Search by address or city!"
+                value={location}
+                onChange={e => setLocation(e.target.value)}
+                />
+                <button type="submit">Search</button>
+            </form>
+        </div>
+        
     )
 }
 
---
+++ src/hooks/yelp-api/config.js
@@ -0,0 +1,9 @@
+export const API_BASE_URL = "https://api.yelp.com/v3/"
+
+// FIX ME: Remove this token
+// NOTE: as long as this token is saved in the frontend, the application is not production ready.
+// The beaerer token must be used on a erver whose inner workings are not exposed to the outside world
+// For simplicity and the sake of this tutorial, I'll just leave it here like this
+// But just bear in mind: NEVER EVER save a bearer token inside the frontend code
+
+expost const BEARER = 
