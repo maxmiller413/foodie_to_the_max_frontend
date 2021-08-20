423183c9e8afe4c18da6776efd0390bd78ceebc0
Max Miller <maxmiller413@gmail.com>
2021-08-20 11:51:39 -0500

    DarkMode

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function DarkMode (){
     return (
-        // ** code from car_finder -- need to work on this **
+        // CAR FINDER CODE
         <>
             {/* <p className={isDarkMode ? 'App' : 'App light'}></p>
             <label className="switch" onClick={() => onToggleDarkMode(onToggleDarkMode => !onToggleDarkMode)}>
