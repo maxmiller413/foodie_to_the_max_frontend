38db595f0a7582fa6d1a03b220ce85f5e853d68c
Max Miller <maxmiller413@gmail.com>
2021-08-24 14:34:57 -0500

    HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,7 @@ import styles from "./HomePage.module.css"
 
 function HomePage({currentUser, addNewWishlistPlace, onSetPlaceId, places, showRes, setShowRes, sortBy, setSortBy, rating, setRating, review, setReview, wishlists, placeId}){
 
-//    *** Homepage ***
+
     // const [places, setPlaces] = useState([])
 
     return(
