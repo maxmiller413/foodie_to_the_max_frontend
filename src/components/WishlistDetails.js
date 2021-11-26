f8796bf34c816e51630ea28110961bd4dc920aa6
Max Miller <maxmiller413@gmail.com>
2021-11-26 14:20:18 -0600

    WishlistDetails

--- src/components/WishlistDetails.js
+++ src/components/WishlistDetails.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom";
-// ???
+// need this???
 import WishlistPlaceCard from "./WishlistPlaceCard"
 
 function WishlistDetails({ wishlist }){
