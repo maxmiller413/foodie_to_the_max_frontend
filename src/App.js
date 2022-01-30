05e6424a81b6d1be64488e1d169ede190f34000e
Max Miller <maxmiller413@gmail.com>
2022-01-29 22:22:11 -0600

    app css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // *** Review with component hierarchy***
 // **** clean up design structure ***
-// update figma & design structure
+// update figma, design structure, css
 
 // import logo from './logo.svg';
 // import './App';
