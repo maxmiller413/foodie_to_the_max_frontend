af22a059a1c6d340a776b95f83e70e22de90c0d0
Max Miller <maxmiller413@gmail.com>
2022-02-05 21:46:05 -0600

    app sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // *** Review with component hierarchy***
 
 // *** clean up hooks ***
-// update figma, design structure, css -- flexbox
+// update figma, design structure, css -- SASS
 
 // import logo from './logo.svg';
 // import './App';
