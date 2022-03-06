07dfa6b46003ae771ce446a51c41982d7a81d6a3
Max Miller <maxmiller413@gmail.com>
2022-03-05 20:35:55 -0600

    WishlistDetails - WishlistPlaceCard defined but not currently used

--- src/components/WishlistDetails.js
+++ src/components/WishlistDetails.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom";
-// ??
+// Review if going to use -- defined but never used
 import WishlistPlaceCard from "./WishlistPlaceCard"
 
 function WishlistDetails({ wishlist }){
